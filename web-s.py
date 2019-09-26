586b7f897522dfd2dd503dae6ec3ff8cf69d6b80
Dhruv <dhrvjsh102189@gmail.com>
2019-09-26 06:08:37 +0530

    nothing new

--- web-s.py
+++ web-s.py
@@ -12,12 +12,12 @@ r = ur.urlopen(urlToScrape).read()
 
 soup = BeautifulSoup(r, "lxml")
 for a in soup.findAll('a', href=True, attrs={'class': '_31qSD5'}):
-	name = a.find('div', attrs={'class':'_3wU53n'})
-	price = a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-	rating = a.find('div', attrs={'class':'hGSR34'})
-	products.append(name.text)
-	prices.append(price.text)
-	ratings.append(rating.text)
+    name = a.find('div', attrs={'class': '_3wU53n'})
+    price = a.find('div', attrs={'class': '_1vC4OE _2rQ-NK'})
+    rating = a.find('div', attrs={'class': 'hGSR34'})
+    products.append(name.text)
+    prices.append(price.text)
+    ratings.append(rating.text)
 
 
 df = pd.DataFrame({'Product Name': products, 'Price': prices, 'Rating': ratings})
